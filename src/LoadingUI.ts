48c337660271ac962a7417ece08596df755bc224
Ocean Stack <lovelypurpleclothes@outlook.com>
2020-08-02 02:33:54 +0800

    resize loadui textfield

--- src/LoadingUI.ts
+++ src/LoadingUI.ts
@@ -5,35 +5,61 @@ class LoadingUI extends egret.Sprite implements RES.PromiseTaskReporter {
   }
 
   private createView(): void {
-    this.graphics.beginFill(0xffffff);
+    this.graphics.beginFill(0x200040);
     this.graphics.drawRect(0, 0, this.stage.stageWidth, this.stage.stageHeight);
     this.graphics.endFill();
 
-    let t = new egret.TextField();
-    t.text = "朱望仔大战大反派";
-    t.size = 50;
-    t.textColor = 0x333333;
-    t.fontFamily = "Microsoft YaHei";
-    t.y = this.stage.stageHeight / 2 / 2;
-    t.x = this.stage.stageWidth / 2;
-    t.anchorOffsetX = t.width / 2;
     let t2 = this.textField = new egret.TextField();
-    t2.textColor = 0x333333;
+    t2.textColor = 0xffce45;
     t2.width = this.stage.stageWidth;
     t2.height = this.stage.stageHeight;
     t2.textAlign = egret.HorizontalAlign.CENTER;
-    this.textField.text = '正在加载...';
+    t2.textFlow = <Array<egret.ITextElement>>[
+      {
+        text: "正在加载...",
+        style: { textColor: 0xffce45, size: 50, stroke: 1, strokeColor: 0xf1f1f1 },
+      },
+      {
+        text: "0",
+        style: { textColor: 0xdd5145, size: 80, stroke: 1, strokeColor: 0xf1f1f1 },
+      },
+      {
+        text: "/",
+        style: { textColor: 0xdd5145, size: 80, stroke: 1, strokeColor: 0xf1f1f1 },
+      },
+      {
+        text: "0",
+        style: { textColor: 0xffce45, size: 50, stroke: 1, strokeColor: 0xf1f1f1 },
+      },
+    ];
 
     t2.y = this.stage.stageHeight / 2;
     t2.x = this.stage.stageWidth / 2;
     t2.anchorOffsetX = t2.width / 2;
-    this.addChild(t);
     this.addChild(t2);
+    egret.Tween.get(t2, { loop: true }).to({ alpha: 0.8, scaleX: 0.8, scaleY: 0.8 }, 300, egret.Ease.bounceOut).wait(200).to({ alpha: 1, scaleX: 1, scaleY: 1 }, 300, egret.Ease.bounceIn);
   }
 
   private textField: egret.TextField;
 
   public onProgress(current: number, total: number): void {
-    this.textField.text = `正在加载...${current}/${total}`;
+    this.textField.textFlow = <Array<egret.ITextElement>>[
+      {
+        text: "正在加载...",
+        style: { textColor: 0xffce45, size: 50, stroke: 1, strokeColor: 0xf1f1f1 },
+      },
+      {
+        text: `${current}`,
+        style: { textColor: 0xdd5145, size: 80, stroke: 1, strokeColor: 0xf1f1f1 },
+      },
+      {
+        text: "/",
+        style: { textColor: 0xdd5145, size: 80, stroke: 1, strokeColor: 0xf1f1f1 },
+      },
+      {
+        text: `${total}`,
+        style: { textColor: 0xffce45, size: 50, stroke: 1, strokeColor: 0xf1f1f1 },
+      },
+    ];
   }
 }
